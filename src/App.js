26d1a228ac12e05d2ef9ad39773db371c67bd976
MamatovaElnoza <mamatovaelnoza@gmail.com>
2025-01-06 16:45:27 +0500

    JSX preprotessor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css'
 
 function App() {
+  const text = 'Hello, Elnoza'
   return (
     <div className="App">
-      <h1>Hello, Elnoza!</h1>
+      <h1>Text: {text}</h1>
+      <input type='text' />
+      <button>click</button>
     </div>
   );
 }
