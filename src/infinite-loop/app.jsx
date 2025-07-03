f0fa2fff167dd27bbc47b0bccc035b790651708e
Elnoza Mamatova <mamatovaelnoza@gmail.com>
2025-07-03 11:25:32 +0500

    infinite loop

---
+++ src/infinite-loop/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './header/header'
+import Home from './home/home'
+import Gallery from './gallery/gallery'
+import Contact from './contact/contact'
+import Footer from './footer/footer'
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+        <Home />
+        <Gallery />
+        <Contact />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
